022bcc731709e98156114bdd8995c676d684ec06
ghostper <144959724+ghostper@users.noreply.github.com>
2025-01-22 13:06:39 +0100

    from block to inactive

--- MOReserve/static/src/php/blockCard.php
+++ MOReserve/static/src/php/blockCard.php
@@ -3,7 +3,7 @@ include "conn.php";
 
 // Decode input
 $input = json_decode(file_get_contents('php://input'), true);
-$status = "block";
+$status = "inactive";
 
 // Check if 'number' is set in the input
 if (!isset($input["number"])) {
